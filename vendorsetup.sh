f0088e126d425753b37c216f89889df8379f6616
hadikhoirudin <347extreme@gmail.comarchlinux-java>
2015-05-15 23:43:49 +0700

    mkvendor.sh: Initial commit of revo_3204

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_revo_3204-eng-userdebbug
+add_lunch_combo cm_revo_3204-eng
+
